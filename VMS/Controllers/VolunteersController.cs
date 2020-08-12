748eed6a2a73982bcdb24750126fd0fad27da62c
Christopher Carter <n01387395@ospreys.unf.edu>
2020-08-12 18:34:27 -0400

    Volunteer section added

---
+++ VMS/Controllers/VolunteersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VMS.Models;
+
+namespace VMS.Controllers
+{
+    public class VolunteersController : Controller
+    {
+        private VolunteerContext db = new VolunteerContext();
+
+        // GET: Volunteers
+        public ActionResult Index()
+        {
+            return View(db.Volunteers.ToList());
+        }
+
+        // GET: Volunteers/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VolunteerModel volunteerModel = db.Volunteers.Find(id);
+            if (volunteerModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(volunteerModel);
+        }
+
+        // GET: Volunteers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Volunteers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Email,FirstName,LastName,Username,Password,Center,Interests,Availability,Address,PhoneNumber,Education,Licenses,EMCname,EMCphone,EMCemail,EMCaddress,DriversLicenseOnFile,SocialCardOnFile,ApprovalStatus")] VolunteerModel volunteerModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Volunteers.Add(volunteerModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(volunteerModel);
+        }
+
+        // GET: Volunteers/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VolunteerModel volunteerModel = db.Volunteers.Find(id);
+            if (volunteerModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(volunteerModel);
+        }
+
+        // POST: Volunteers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Email,FirstName,LastName,Username,Password,Center,Interests,Availability,Address,PhoneNumber,Education,Licenses,EMCname,EMCphone,EMCemail,EMCaddress,DriversLicenseOnFile,SocialCardOnFile,ApprovalStatus")] VolunteerModel volunteerModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(volunteerModel).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(volunteerModel);
+        }
+
+        // GET: Volunteers/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VolunteerModel volunteerModel = db.Volunteers.Find(id);
+            if (volunteerModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(volunteerModel);
+        }
+
+        // POST: Volunteers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            VolunteerModel volunteerModel = db.Volunteers.Find(id);
+            db.Volunteers.Remove(volunteerModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ VMS/Models/VolunteerModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace VMS.Models
+{
+    public class VolunteerModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Center { get; set; }
+        public string Interests { get; set; }
+        public string Availability { get; set; } //How to handle this??
+        public string Address { get; set; }  //How to handle this??
+        public int PhoneNumber { get; set; }
+        [Key]
+        public string Email { get; set; }
+        public string Education { get; set; }
+        public string Licenses { get; set; }
+        public string EMCname { get; set; }
+        public int EMCphone { get; set; }
+        public string EMCemail { get; set; }
+        public string EMCaddress { get; set; }  //How to handle this??
+        public bool DriversLicenseOnFile { get; set; }
+        public bool SocialCardOnFile { get; set; }
+        public string ApprovalStatus { get; set; }
+    }
+
+    public class VolunteerContext : DbContext
+    {
+        public VolunteerContext() : base("VMSdb")
+        {
+
+        }
+
+        public DbSet<VolunteerModel> Volunteers { get; set; }
+    }
+}
